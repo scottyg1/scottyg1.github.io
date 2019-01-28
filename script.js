3c7f9a49a51f5979dd43064c8d6eabbd727082be
scottyg1 <45944801+scottyg1@users.noreply.github.com>
2019-01-28 12:32:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ function gbpfunc()
 init();
     
     
+// mouseX and mouseY
+
+strokeWeight(3);
+stroke(57, 0, 214);
+fill(0, 210, 247);
+
+draw = function() {
+    
+    
+    background(255, 255, 255);
+    
+    ellipse(mouseY, mouseX, 30, 30);
+    
+};
